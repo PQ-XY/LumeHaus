6e95d882402fc9f80fef0a3216b54d31f5ec45d1
Yao Xu <yaoxu369@gmail.com>
2025-05-26 17:38:22 -0400

    deployment setup 2

--- backend/go/src/estore/constants/constants.go
+++ backend/go/src/estore/constants/constants.go
@@ -3,5 +3,4 @@ package constants
 const (
     APP_INDEX = "app"
     USER_INDEX = "user"
-	STRIPE_API_KEY = "sk_test_51RSOJTRsnm5sFP5pAzI5cEEfbMm7p2DOlzx6CXQNzFl4aSMsBdAXUPV77GlmbMRJmrynCOqe5B8k9lShHUz7mI3M00Zp2Cff0b"
 )
